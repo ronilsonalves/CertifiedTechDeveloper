5b86237d842ac158c680b4952bee3764c03fcf25
ronilsonalves <falecom@ronilsonalves.com>
2021-06-11 17:14:14 -0300

    Add folder 'PIM - Turma 5/Aula04' and files fluxoMeuPrimeiroIf.js & fluxoIgualdadeIfElse.js

---
+++ PIM - Turma 5/Aula04/fluxoIgualdadeIfElse.js
@@ -0,0 +1,13 @@
+/* Exercício "Igualdade, If e Else" - Aula Assíncrona 04
+   Assunto: Controle de Fluxo
+ - Ronilson Alves - */
+
+ // Declaração de variável e atribuição de dados
+ let linguagem = "JavaScript";
+
+ // Construção do bloco condicional
+ if (linguagem == "JavaScript") {
+     console.log("Estou aprendendo",linguagem);
+ } else {
+     console.log("Aprenderei mais tarde",linguagem);
+ }
---
+++ PIM - Turma 5/Aula04/fluxoMeuPrimeiroIf.js
@@ -0,0 +1,13 @@
+/* Exercício do meu primeiro if - Aula Assíncrona 04
+   Assunto: Controle de Fluxo
+ - Ronilson Alves - */
+
+ // Declaração de variável e atribuição de dados
+ let status = true;
+
+ // Construção do bloco condicional
+ if (status == true) {
+     console.log("O valor é",status,"- Verdadeiro")
+ } else {
+     console.log("O valor é",status," - Falso")
+ }
